ac0d2090178d68c459e433418e7488e4d2c4f0e6
Dochkin-sergey <96723560+Dochkin-sergey@users.noreply.github.com>
2024-06-08 12:30:06 +0300

    Add Page Object

---
+++ src/test/java/org/example/selenium/pages/ExpectationsAndTabNavigation.java
@@ -0,0 +1,33 @@
+package org.example.selenium.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class ExpectationsAndTabNavigation {
+    private WebDriver driver;
+
+    public void expectation(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))));
+                System.out.println("Ожидание до появления элементов");
+    }
+
+    public void switchTab(){
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1).toString());
+        System.out.println("Переключение вкладки");
+    }
+
+    public ExpectationsAndTabNavigation (WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ src/test/java/org/example/selenium/tests/TestBingo.java
@@ -0,0 +1,72 @@
+package org.example.selenium.tests;
+
+import org.example.selenium.pages.ExpectationsAndTabNavigation;
+import org.example.selenium.pages.MainPage;
+import org.example.selenium.pages.ResultsPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBingo {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void searchFieldTest() {
+        String input = "Selenium";
+
+        MainPage mp = new MainPage(driver);
+        mp.sendText(input);
+
+        ResultsPage rp = new ResultsPage(driver);
+        assertEquals(input, rp.getTextSearchField(), "Текст не совпал");
+    }
+
+    @DisplayName("URL")
+    @Test
+    public void searchResultTest() {
+        String input = "Selenium";
+        MainPage mp = new MainPage(driver);
+        mp.sendText(input);
+
+        ExpectationsAndTabNavigation ost = new ExpectationsAndTabNavigation(driver);
+        ost.expectation();
+
+        ResultsPage rp = new ResultsPage(driver);
+        rp.clickElement(0);
+        ost.switchTab();
+
+        String URL = driver.getCurrentUrl();
+        assertEquals(URL, "https://www.selenium.dev/");
+    }
+}
